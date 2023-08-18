75e459145d1511e9b5e655b44d90076978e868d7
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-18 13:46:16 -0300

    Add ckan installation but sudo not working

--- Vagrantfile
+++ Vagrantfile
@@ -8,8 +8,9 @@ Vagrant.configure("2") do |config|
   config.env.enable
 
   config.vm.define "ckan" do |ckan|
-    ckan.vm.box = "ubuntu/bionic64"
+    ckan.vm.box = "ubuntu/focal64"
     ckan.vm.provision "shell", path: "setup/provision_ckan.sh", env: {VAGRANT_HTTP_PROXY:ENV['VAGRANT_HTTP_PROXY'], VAGRANT_HTTPS_PROXY:ENV['VAGRANT_HTTPS_PROXY']}
+    ckan.vm.provision "shell", inline: 'echo ". /usr/lib/ckan/default/bin/activate && cd /vagrant" > ~/.profile', privileged: false
   end
 
 end
--- setup/provision_ckan.sh
+++ setup/provision_ckan.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "-----------------------------------------------"
 echo "Initial setup.  This may take a few minutes ..."
+echo "-----------------------------------------------"
 
+echo "-----------------------------------------------"
 echo "Installing linux packages."
+echo "-----------------------------------------------"
 sudo apt-get update
 sudo apt-get install -y python3-dev \
                    postgresql \
@@ -14,11 +18,32 @@ sudo apt-get install -y python3-dev \
                    redis-server \
                    graphviz
 
-echo "Java update"
+echo "-----------------------------------------------"
+echo "Java update."
+echo "-----------------------------------------------"
 sudo apt-get update
 sudo apt-get install -y default-jdk
 sudo apt-get update
 sudo apt-get install -y default-jre
+
+echo "-----------------------------------------------"
+echo "Install CKAN into a Python virtual environment."
+echo "-----------------------------------------------"
+mkdir -p ~/ckan/lib
+sudo ln -s ~/ckan/lib /usr/lib/ckan
+mkdir -p ~/ckan/etc
+sudo ln -s ~/ckan/etc /etc/ckan
+sudo mkdir -p /usr/lib/ckan/default
+sudo chown `whoami` /usr/lib/ckan/default
+python3 -m venv /usr/lib/ckan/default
+. /usr/lib/ckan/default/bin/activate
+pip install setuptools==44.1.0
+pip install --upgrade pip
+git clone https://github.com/ckan/ckan.git ckan_source
+cd ckan_source/
+git checkout ckan-2.10.1
+pip install -r requirements.txt
+python setup.py install
 
 echo "The environment has been installed."
 echo
