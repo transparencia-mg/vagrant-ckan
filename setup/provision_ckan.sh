4a91b96c1fc41c1721c0ba03e9ba9b50bf3477e0
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-22 17:39:02 -0300

    Tentativa de resolver problema solr
    See #2

--- setup/provision_ckan.sh
+++ setup/provision_ckan.sh
@@ -5,7 +5,7 @@ echo "Initial setup.  This may take a few minutes ..."
 echo "-----------------------------------------------"
 
 echo "-----------------------------------------------"
-echo "Installing linux packages."
+echo "Install the required packages."
 echo "-----------------------------------------------"
 sudo apt-get update
 sudo apt-get install -y python3-dev \
@@ -19,17 +19,8 @@ sudo apt-get install -y python3-dev \
                    graphviz
 
 echo "-----------------------------------------------"
-echo "Java update."
-echo "-----------------------------------------------"
-sudo apt-get update
-sudo apt-get install -y default-jdk
-sudo apt-get update
-sudo apt-get install -y default-jre
-
-echo "-----------------------------------------------"
 echo "Install CKAN into a Python virtual environment."
 echo "-----------------------------------------------"
-# Install inside virtual environment
 mkdir -p ~/ckan/lib
 sudo ln -s ~/ckan/lib /usr/lib/ckan
 mkdir -p ~/ckan/etc
@@ -40,44 +31,51 @@ python3 -m venv /usr/lib/ckan/default
 . /usr/lib/ckan/default/bin/activate
 pip install setuptools==44.1.0
 pip install --upgrade pip
-# CKAN clone installation
-git clone https://github.com/ckan/ckan.git ckan_source
-cd ckan_source/
-git checkout ckan-2.10.1
-pip install -r requirements.txt
-python setup.py install
-# CKAN config file
-sudo mkdir -p /etc/ckan/default
-sudo chown -R `whoami` /etc/ckan/
-ckan generate config /etc/ckan/default/ckan.ini
-cd ..
+pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.10.1#egg=ckan[requirements]'
+deactivate
+. /usr/lib/ckan/default/bin/activate
 
 echo "-----------------------------------------------"
-echo "Postgres configuration."
+echo "Setup a PostgreSQL database."
 echo "-----------------------------------------------"
 sudo service postgresql start
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'ckan_default';"
 sudo -u postgres psql -c "CREATE DATABASE ckan_default WITH OWNER = 'ckan_default' ENCODING 'utf-8'"
+sudo -u postgres psql -l
+
+echo "-----------------------------------------------"
+echo "Create a CKAN config file."
+echo "-----------------------------------------------"
+sudo mkdir -p /etc/ckan/default
+sudo chown -R `whoami` /etc/ckan/
+ckan generate config /etc/ckan/default/ckan.ini
+# ckan.ini configuration
+ckanFile=/etc/ckan/default/ckan.ini
+# Change sqlalchemy.ur
+lineNumber=$(grep --line-number "sqlalchemy.url" $ckanFile  | cut -f1 -d:)
+replacedLine="sqlalchemy.url = postgresql://ckan_default:ckan_default@localhost/ckan_default"
+replacedLine=${replacedLine//\//\\\/}
+sed -i $lineNumber's/.*/'"$replacedLine"'/' $ckanFile
+# solr_url
+lineNumber=$(grep --line-number "solr_url" $ckanFile  | cut -f1 -d:)
+replacedLine="solr_url = http://127.0.0.1:8983/solr/ckan/"
+replacedLine=${replacedLine//\//\\\/}
+sudo sed -i $lineNumber's/.*/'"$replacedLine"'/' $ckanFile
+
 
 echo "-----------------------------------------------"
 echo "Install and Setup solr."
 echo "-----------------------------------------------"
-cd /opt
 sudo wget https://www.apache.org/dyn/closer.lua/lucene/solr/8.11.2/solr-8.11.2.tgz?action=download
 sudo mv solr-8.11.2.tgz\?action\=download solr-8.11.2.tgz
 sudo tar xzf solr-8.11.2.tgz solr-8.11.2/bin/install_solr_service.sh --strip-components=2
 sudo bash ./install_solr_service.sh solr-8.11.2.tgz
-sudo sudo service solr status
+sudo service solr status
 sudo -u solr /opt/solr/bin/solr create -c ckan
-sudo rm -rf /var/solr/data/ckan/conf/managed-schema.xml
-sudo ln -s /home/vagrant/ckan_source/ckan/config/solr/schema.xml /var/solr/data/ckan/conf/managed-schema.xml
-sudo chown -R solr:solr /var/solr
-ckanFile=/etc/ckan/default/ckan.ini
-lineNumber=$(grep --line-number "solr_url" $ckanFile  | cut -f1 -d:)
-replacedLine="solr_url = http://127.0.0.1:8983/solr/ckan/"
-replacedLine=${replacedLine//\//\\\/}
-sudo sed -i $lineNumber's/.*/'"$replacedLine"'/'  $ckanFile
+sudo -u solr wget -O /var/solr/data/ckan/conf/managed-schema https://raw.githubusercontent.com/ckan/ckan/dev-v2.10/ckan/config/solr/schema.xml
+sudo service solr restart
 wget http://localhost:8983/solr
+
 
 echo "-----------------------------------------------"
 echo "Start redis."
@@ -85,13 +83,9 @@ echo "-----------------------------------------------"
 sudo service redis-server start
 
 echo "-----------------------------------------------"
-echo "Configure db on ckan.ini and create tables."
+echo "Create database tables."
 echo "-----------------------------------------------"
-ckanFile=/etc/ckan/default/ckan.ini
-lineNumber=$(grep --line-number "sqlalchemy.url" $ckanFile  | cut -f1 -d:)
-replacedLine="sqlalchemy.url = postgresql://ckan_default:ckan_default@localhost/ckan_default"
-replacedLine=${replacedLine//\//\\\/}
-sed -i $lineNumber's/.*/'"$replacedLine"'/'  $ckanFile
+cd /usr/lib/ckan/default/src/ckan
 ckan -c /etc/ckan/default/ckan.ini db init
 
 echo "-----------------------------------------------"
